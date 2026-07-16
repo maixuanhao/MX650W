38e3101978fead326764163465d9d306420dd499
maixuanhao <46432287+maixuanhao@users.noreply.github.com>
2026-07-16 19:10:12 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# OpenWrt DIY script part 1 (Before Update feeds)
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Thêm kho gói chứa các app còn thiếu (bao gồm Turboacc, ddnsto, dnsfilter...)
+echo 'src-git extra https://github.com/shiyu888/to-packages.git;main' >> feeds.conf.default
+echo 'src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package.git;main' >> feeds.conf.default
