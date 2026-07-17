ce2d8b73239135f3745caed7bbacfa13ccaadf8e
maixuanhao <46432287+maixuanhao@users.noreply.github.com>
2026-07-17 11:54:53 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# OpenWrt DIY script part 1 (Before Update feeds)
 
-# Thêm kho gói chứa các app còn thiếu (bao gồm Turboacc, ddnsto, dnsfilter...)
-echo 'src-git extra https://github.com/shiyu888/to-packages.git;main' >> feeds.conf.default
-echo 'src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package.git;main' >> feeds.conf.default
+# Vô hiệu hóa triệt để cấu hình gọi đến hai kho feeds lỗi
+if [ -f feeds.conf.default ]; then
+    sed -i '/extra/d' feeds.conf.default
+    sed -i '/sirpdboy/d' feeds.conf.default
+fi
+
+# Tạo thư mục giả lập để đánh lừa lệnh 'find' của OpenWrt không bị báo lỗi thiếu thư mục
+mkdir -p feeds/extra
+mkdir -p feeds/sirpdboy
+touch feeds/extra/.placeholder
+touch feeds/sirpdboy/.placeholder
+
+# Ép hệ thống tạo sẵn file index trống để không bị crash ở bước tiếp theo
+touch feeds/extra.index
+touch feeds/sirpdboy.index
