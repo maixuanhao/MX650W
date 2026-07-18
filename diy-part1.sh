6be113ab534686e342c1ab6c31a2a4e7df0f97a8
maixuanhao <46432287+maixuanhao@users.noreply.github.com>
2026-07-18 10:30:50 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# Vô hiệu hóa triệt để cấu hình gọi đến hai kho feeds lỗi
-if [ -f feeds.conf.default ]; then
-    sed -i '/extra/d' feeds.conf.default
-    sed -i '/sirpdboy/d' feeds.conf.default
-fi
-
-# Tạo thư mục giả lập để đánh lừa lệnh 'find' của OpenWrt không bị báo lỗi thiếu thư mục
-mkdir -p feeds/extra
-mkdir -p feeds/sirpdboy
-touch feeds/extra/.placeholder
-touch feeds/sirpdboy/.placeholder
-
-# Ép hệ thống tạo sẵn file index trống để không bị crash ở bước tiếp theo
-touch feeds/extra.index
-touch feeds/sirpdboy.index
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
