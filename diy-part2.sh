a9245787c596e820b24eae180e2058ae03a009c3
maixuanhao <46432287+maixuanhao@users.noreply.github.com>
2026-07-20 06:02:57 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,4 +4,7 @@
         rm -f openwrt/.config
         chmod +x $DIY_P2_SH
         cd openwrt
+        # Chạy script để điền các cấu hình của bạn
         bash ../$DIY_P2_SH
+        # BẮT BUỘC: Tạo file cấu hình chuẩn dựa trên các tùy chọn trên
+        make defconfig
