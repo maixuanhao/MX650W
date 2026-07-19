f8c90ae24bb029de3e664f2594f11373957989b4
maixuanhao <46432287+maixuanhao@users.noreply.github.com>
2026-07-19 22:19:28 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+src-git packages https://git.openwrt.org/openwrt/packages.git
+src-git luci https://git.openwrt.org/openwrt/luci.git
+src-git routing https://git.openwrt.org/openwrt/routing.git
+src-git telephony https://git.openwrt.org/openwrt/telephony.git
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small
